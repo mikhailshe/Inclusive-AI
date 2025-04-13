b20aa1e7aac44adbfc408c6e48d5f3b387d29760
mikhailshe <michelshevnin@gmail.com>
2025-04-13 17:53:41 +0300

    Updated routes.

--- inclusive_ai/settings/urls.py
+++ inclusive_ai/settings/urls.py
@@ -1,8 +1,27 @@
+from django import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    # --------------- DJANGO URL's ----------------
     path('dev/admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('', include('main.urls')),
+    path('media/', views.static.serve),
+
+    # --------------- OTHER URL's -----------------
+    path('accounts/', include('allauth.urls')),
+
+    # -------------- PROJECT URL's ----------------
+    path('', include('apps.pages.urls')),
+    path('pears/', include('apps.pears.urls')),
 ]
+
+
+# --------------------------- URLS EXTENDED ----------------------------
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
